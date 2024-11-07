e4aba1cc08bfad71c5d5b380e81f270b9d51e431
Aluno <alexandre.milani@acad.ufsm.br>
2024-11-06 21:20:54 -0300

    Aula 06/11

--- src/app/app-core/servicos/tarefa-service.service.ts
+++ src/app/app-core/servicos/tarefa-service.service.ts
@@ -8,7 +8,7 @@ export class TarefaService {
   private listaDeTarefas: string[] = [];
   constructor() { }
 
-  addtarefa(tarefa: string){
+  addTarefa(tarefa: string){
     this.listaDeTarefas.push(tarefa);
     console.log("TAREFAS ADICIONADAS",
       this.listaDeTarefas);
--- src/app/componentes/visualizar-tarefas/visualizar-tarefas.component.ts
+++ src/app/componentes/visualizar-tarefas/visualizar-tarefas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {TarefaService} from "../../app-core/servicos/tarefa-service.service";
 
+declare var $: any;
+
 @Component({
   selector: 'app-visualizar-tarefas',
   templateUrl: './visualizar-tarefas.component.html',
@@ -8,9 +10,25 @@ import {TarefaService} from "../../app-core/servicos/tarefa-service.service";
 })
 export class VisualizarTarefasComponent implements OnInit {
 
+  i: number =0;
   constructor(private tarefaService: TarefaService) { }
+
 
   ngOnInit(): void {
   }
 
+
+  addTarefa(){
+    this.tarefaService.addTarefa("TAREFA " + this.i);
+    this.i ++;
+  }
+
+
+  openModal(){
+    $('#add-tarefa').modal('show');
+  }
+
+  closeModal(){
+    $('#add-tarefa').modal('hide');
+  }
 }
